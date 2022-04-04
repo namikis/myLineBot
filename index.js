f1e9f825395136beb072433f938944c906be8c3e
tairyu saito <tairyu@tairyunoMacBook-Pro.local>
2022-04-04 23:23:35 +0900

    test conv

--- index.js
+++ index.js
@@ -10,7 +10,27 @@ server.listen(process.env.PORT || 3000, () => {
     console.log("server listening...");
 });
 
+const bot = new line.Client(line_config);
+
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
-    console.log(req.body);
+    
+    let events_processed = [];
+
+    req.body.events.forEach((event) => {
+        if(event.type == "message" && event.message.type == "text"){
+            if(event.message.text == "こんにちは"){
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "うるせえ！"
+                }))
+            }
+        }
+    })
+
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} events processed`);
+        }
+    );
 });
