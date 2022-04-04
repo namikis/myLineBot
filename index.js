0801b25fdd66cabe4e8bf0bfa386932baecd2fec
tairyu saito <tairyu@tairyunoMacBook-Pro.local>
2022-04-04 23:06:55 +0900

    fix: miss2

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const server = require("express")();
-const line = require("@line/bot-sdk")
+const line = require("@line/bot-sdk");
 
 const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET
-}
+};
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => {
+    console.log("server listening...");
+});
 
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
     console.log(req.body);
-})
+});
